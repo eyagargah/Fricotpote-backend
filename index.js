73d12a6df83adf043b72fdfe2394039db820fa4c
Eya Gargah <eyagargah@hotmail.com>
2024-03-13 00:12:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const uri =  process.env.uri;
 const port = process.env.port;
 /*******Cors********/
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
